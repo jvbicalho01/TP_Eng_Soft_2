737cf42bb202c202187ca576c3e9f82f67055630
João Vitor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2023-10-29 12:40:55 -0300

    add TrasactionsTable component

--- gerenciador-financas/src/components/Dashboard/index.jsx
+++ gerenciador-financas/src/components/Dashboard/index.jsx
@@ -1,12 +1,14 @@
 import { Container } from "./styles"
 
 import Summary from "../Summary"
+import TransactionsTable from "../TransactionsTable"
 
 function Dashboard() {
 
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 
